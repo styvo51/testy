25f267c876c447921a5b75935d8740db12c4f1ed
Lucas Cullen <bitcoinbrisbane@users.noreply.github.com>
2019-10-03 06:14:43 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ cd imx
 git stash
 git checkout master
 git pull
-cp imx.service /
+cp imx.service /etc/systemd/system/imx.service
 sudo dotnet publish -c Release -o /var/www/imx/
 sudo systemctl restart imx.service
+
+sudo systemctl enable instantcrypto.service
