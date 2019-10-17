96386b3979b9af15aba237bdc994f5348f890876
Justin Taylor <s.justin.taylor@gmail.com>
2019-10-17 16:15:15 +1000

    added updated csv upload scripts

--- mass-upload-scripts/csv-upload-noheader.py
+++ mass-upload-scripts/csv-upload-noheader.py
@@ -19,21 +19,14 @@ conn_string = "host="+ config['PGHOST'] +" port="+ config['PGPORT'] +" dbname="+
 conn=psycopg2.connect(conn_string)
 print("Connected!")
 cursor = conn.cursor()
-
-#cursor.execute("INSERT INTO "+config.PGTABLE +" (title, first_name, last_name, email, address, address2, postcode, dob, telephone, url, ip) VALUES (" +")");
-#f = open(config['TARGETFILE'], 'r')
-#cursor.copy_from(f, config['PGTABLE'], sep=',')
-#f.close()
-
-#with open(config['TARGETFILE'], 'r') as f:
-#	cursor.copy_from(f, config['PGTABLE'], sep=',',null='', columns=('title', 'first_name','last_name','email', 'address', 'address2','postcode', 'dob', 'telephone', 'url','ip'))
-queryString = f'INSERT INTO {config["PGTABLE"]} (title, first_name, last_name, email, address, address2, postcode, dob, telephone, url, ip) VALUES(%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s)'
+queryString = f'INSERT INTO {config["PGTABLE"]} (title, first_name, last_name, email, address, address2, postcode, dob, telephone, url, ip) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
 recordCount = 0
 with open(config['TARGETFILE'], 'r') as f:
 	csv = csv.reader(f, delimiter=',')    
 	for row in csv:
 		cursor.execute(queryString,(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10]))
 		recordCount += 1
+		print(f'{recordCount}: {row[0]}, {row[1]}, {row[2]}, {row[3]}, {row[4]}, {row[5]}, {row[6]}, {row[7]}, {row[8]}, {row[9]}, {row[10]}')
 	
 conn.commit()
 print(f'{recordCount} records inserted successfully')
